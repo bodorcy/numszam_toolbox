37ba8d53148fb90d6adb72582ae3ca3a1fa46a5f
Vincze Nándor <h379409@stud.u-szeged.hu>
2025-05-17 22:19:59 +0200

    output formatting, gauss-seidel

--- web/problems/generators.py
+++ web/problems/generators.py
@@ -1,6 +1,8 @@
 import numpy as np
 import decomp.cholesky as cholesky
 from decomp.lu import lu
+from decomp.qr import qr
+from iterative.gauss_seidel import gauss_seidel
 from iterative.power import power_method
 from utils.matrix_utils import get_random_regular_matrix, get_random_positive_def_matrix, pretty_print_matrix
 from poly.polynomial import LagrangePolynomial, generate_base_points
@@ -12,21 +14,27 @@ def generate_lu():
 
     A = get_random_regular_matrix(n)
 
-    L,U = lu(A)
+    L, U, output = lu(A, verbose=True)
+
+    output_str = "\n".join(output)
 
     return (
         f"Végezdd el az LU felbontást a következő mátrixon:\n"
         f"{pretty_print_matrix(A)}\n.",
-        f"L =\n{pretty_print_matrix(L)}\nU =\n{pretty_print_matrix(U)}"
+        f"L =\n{pretty_print_matrix(L)}\n\nU =\n{pretty_print_matrix(U)}"
+        f"\n\n{output_str}"
+
     )
 def generate_qr():
     n = np.random.randint(2,4)
     A = get_random_regular_matrix(n)
-    Q, R = np.linalg.qr(A)
+    Q, R, output = qr(A, True)
+
     return (
         f"Végezdd el a QR felbontást a következő mátrixon:\n"
         f"{pretty_print_matrix(A)}\n.",
         f"Q =\n{pretty_print_matrix(Q)}\nR =\n{pretty_print_matrix(R)}"
+        f"\n\n{output}"
     )
 def generate_cholesky():
     n = np.random.randint(2, 4)
@@ -45,9 +53,9 @@ def generate_power():
     eigenvalue, eigenvector, out = power_method(A, v0=v0, max_iterations=iterations+1, verbose=True)
 
     return (
-        f"Hajts végre az alábbi kezdővektoron (x0) és mátrixon a hatványmódszermódszerrel {iterations} iterációt!\n\
-        Mi a spektrálsugár közelítése ennyi itaráció után?\nx0 =\n{pretty_print_matrix(v0, 0)}\n\
-        A =\n{pretty_print_matrix(A)}\n",
+        f"Hajts végre az alábbi kezdővektoron (x0) és mátrixon a hatványmódszermódszerrel {iterations} iterációt!\n"
+        f"Mi a spektrálsugár közelítése ennyi itaráció után?\nx0 =\n{pretty_print_matrix(v0, 0)}\n"
+        f"A =\n{pretty_print_matrix(A)}\n",
         f"Spektrálsugár közelítés: {np.round(eigenvalue, 2)}\nIterációk: {out}"
     )
 def generate_lagrange():
@@ -78,6 +86,24 @@ def generate_lagrange():
         f"Részpolinomok:\n{Li_str}",
         path
     )
+def generate_gauss_seidel():
+    n = np.random.randint(2, 4)
+    A = get_random_regular_matrix(n)
+    b = np.random.randint(-10, 10, size=(n, 1))
+    x0 = np.zeros((n, 1))
+    iterations = 3
+
+    steps = gauss_seidel(A, b, x0, iterations)
+
+    solution_text = ""
+    for i, step in enumerate(steps, 1):
+        solution_text += f"Iteráció {i}:\n{pretty_print_matrix(step)}\n\n"
+
+    return (
+        f"Közelítsd alábbi lineáris egyenletrendszer megoldását Gauss-Seidel iterációval ({iterations} lépés):\n"
+        f"A =\n{pretty_print_matrix(A)}\nb =\n{pretty_print_matrix(b)}\nx0 =\n{pretty_print_matrix(x0)}",
+        solution_text
+    )
 
 
 def generate_problem(topic):
@@ -86,7 +112,8 @@ def generate_problem(topic):
         "qr": generate_qr,
         "cholesky": generate_cholesky,
         "power": generate_power,
-        "lagrange": generate_lagrange
+        "lagrange": generate_lagrange,
+        "gauss_seidel": generate_gauss_seidel
     }
 
     if topic not in generators:
